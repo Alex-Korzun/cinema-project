41fd7075fc1c732eff334b3fd9e973ce766c3d18
Alex Korzun <koleksandr4@gmail.com>
2021-01-26 16:39:45 +0200

    added hibernate.cfg

--- src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
@@ -1,20 +1,25 @@
 package com.dev.cinema.dao.impl;
 
 import com.dev.cinema.dao.MovieDao;
+import com.dev.cinema.exception.DataProcessingException;
 import com.dev.cinema.lib.Dao;
 import com.dev.cinema.model.Movie;
 import com.dev.cinema.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 import java.util.Collections;
 import java.util.List;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
-        @Override
+    @Override
     public Movie add(Movie movie) {
         Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
             Long itemId = (Long) session.save(movie);
             transaction.commit();
@@ -24,12 +29,19 @@ public class MovieDaoImpl implements MovieDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't insert Movie entity", e);
+            throw new DataProcessingException("Can't insert Movie entity", e);
+        } finally {
+            session.close();
         }
     }
 
     @Override
     public List<Movie> getAll() {
-        return Collections.emptyList();
+        try (Session session = HibernateUtil.getSessionFactory().openSession();){
+            Query<Movie> getAllMoviesQuery = session.createQuery("FROM Movie", Movie.class);
+            return getAllMoviesQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get Movies", e);
+        }
     }
 }
