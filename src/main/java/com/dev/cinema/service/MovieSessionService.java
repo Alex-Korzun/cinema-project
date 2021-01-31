65319d3b8493f51d1344c7241337a99cf69eb668
Alex Korzun <koleksandr4@gmail.com>
2021-01-31 18:27:47 +0200

    created services for CinemaHall and MovieSession

--- src/main/java/com/dev/cinema/service/MovieSessionService.java
+++ src/main/java/com/dev/cinema/service/MovieSessionService.java
@@ -1,10 +1,10 @@
-package com.dev.cinema.dao;
+package com.dev.cinema.service;
 
 import com.dev.cinema.model.MovieSession;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface MovieSessionDao {
+public interface MovieSessionService {
     MovieSession add(MovieSession session);
 
     List<MovieSession> findAvailableSessions(Long movieId, LocalDate date);
