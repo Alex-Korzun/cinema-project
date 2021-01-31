c33ed512f2b7e1a29626c7eabd4bfa4c22e2b559
Alex Korzun <koleksandr4@gmail.com>
2021-01-31 18:31:46 +0200

    created implementations for 'CinemaHallService' and 'MovieSessionService'

--- src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
@@ -2,6 +2,7 @@ package com.dev.cinema.dao.impl;
 
 import com.dev.cinema.dao.MovieSessionDao;
 import com.dev.cinema.exception.DataProcessingException;
+import com.dev.cinema.lib.Dao;
 import com.dev.cinema.model.MovieSession;
 import com.dev.cinema.util.HibernateUtil;
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+@Dao
 public class MovieSessionDaoImpl implements MovieSessionDao {
     @Override
     public MovieSession add(MovieSession movieSession) {
