5339f6dc14342564e50d4dc2b2d0d11fe4281b37
Alex Korzun <koleksandr4@gmail.com>
2021-01-31 18:25:04 +0200

    created 'MovieSessionDaoImpl'

---
+++ src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,50 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.MovieSessionDao;
+import com.dev.cinema.exception.DataProcessingException;
+import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.util.HibernateUtil;
+import java.time.LocalDate;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert Movie Session entity" + movieSession, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("select ms from MovieSession ms "
+                    + "join fetch ms.movie.id "
+                    + "join fetch ms.movie.movie "
+                    + "where ms.movie.id = :movie_id "
+                    + "and ms.date = :date", MovieSession.class)
+                .setParameter("movie_id", movieId)
+                .setParameter("date", date)
+                .getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find available Sessions for "
+                    + movieId + date.toString(), e);
+        }
+    }
+}
