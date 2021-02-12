8d71815655125b939285133a131c1a70b53fba9a
Alex Korzun <koleksandr4@gmail.com>
2021-02-12 10:34:07 +0200

    removed @ResponseBody

--- src/main/java/com/dev/cinema/controller/HelloController.java
+++ src/main/java/com/dev/cinema/controller/HelloController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @ResponseBody
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello";
