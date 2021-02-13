57032fe83778111fa2e1e41f7a1bc6346ca0a171
Alex Korzun <koleksandr4@gmail.com>
2021-02-13 17:28:16 +0200

    fixed 'MovieSessioMapper'

--- src/main/java/com/dev/cinema/service/mapper/MovieSessionMapper.java
+++ src/main/java/com/dev/cinema/service/mapper/MovieSessionMapper.java
@@ -3,17 +3,23 @@ package com.dev.cinema.service.mapper;
 import com.dev.cinema.model.MovieSession;
 import com.dev.cinema.model.dto.request.MovieSessionRequestDto;
 import com.dev.cinema.model.dto.response.MovieSessionResponseDto;
+import com.dev.cinema.service.CinemaHallService;
 import com.dev.cinema.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Component
 public class MovieSessionMapper {
     private final MovieService movieService;
+    private final CinemaHallService cinemaHallService;
 
     @Autowired
-    public MovieSessionMapper(MovieService movieService) {
+    public MovieSessionMapper(MovieService movieService, CinemaHallService cinemaHallService) {
         this.movieService = movieService;
+        this.cinemaHallService = cinemaHallService;
     }
 
     public MovieSessionResponseDto toDto(MovieSession movieSession) {
@@ -29,6 +35,10 @@ public class MovieSessionMapper {
     public MovieSession fromDto(MovieSessionRequestDto movieSessionRequestDto) {
         MovieSession movieSession = new MovieSession();
         movieSession.setId(movieSessionRequestDto.getId());
-        movieSession.setMovie(movieService.);
+        movieSession.setMovie(movieService.get(movieSessionRequestDto.getMovieId()));
+        movieSession.setCinemaHall(cinemaHallService.get(movieSessionRequestDto.getCinemaHallId()));
+        LocalDateTime localDateTime = LocalDateTime.parse(movieSessionRequestDto.getShowTime(), DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        movieSession.setShowTime(localDateTime);
+        return movieSession;
     }
 }
