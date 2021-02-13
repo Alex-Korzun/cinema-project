5eb7d839ee4e7a81d59541c2987ed645511eca33
Alex Korzun <koleksandr4@gmail.com>
2021-02-13 15:47:17 +0200

    added 'request' and 'response' Dtos for all requested models

---
+++ src/main/java/com/dev/cinema/model/dto/response/MovieSessionResponseDto.java
@@ -0,0 +1,49 @@
+package com.dev.cinema.model.dto.response;
+
+public class MovieSessionResponseDto {
+    private Long movieSessionId;
+    private String movieTitle;
+    private String showTime;
+    private Long cinemaHallId;
+    private int cinemaHallCapacity;
+
+    public Long getMovieSessionId() {
+        return movieSessionId;
+    }
+
+    public void setMovieSessionId(Long movieSessionId) {
+        this.movieSessionId = movieSessionId;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public String getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(String showTime) {
+        this.showTime = showTime;
+    }
+
+    public Long getCinemaHallId() {
+        return cinemaHallId;
+    }
+
+    public void setCinemaHallId(Long cinemaHallId) {
+        this.cinemaHallId = cinemaHallId;
+    }
+
+    public int getCinemaHallCapacity() {
+        return cinemaHallCapacity;
+    }
+
+    public void setCinemaHallCapacity(int cinemaHallCapacity) {
+        this.cinemaHallCapacity = cinemaHallCapacity;
+    }
+}
