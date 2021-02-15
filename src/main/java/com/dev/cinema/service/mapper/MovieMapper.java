b5def5a71bb54ba5448f16dc2e945aa10e6d3d4c
Alex Korzun <koleksandr4@gmail.com>
2021-02-15 14:41:55 +0200

    minor fix

--- src/main/java/com/dev/cinema/service/mapper/MovieMapper.java
+++ src/main/java/com/dev/cinema/service/mapper/MovieMapper.java
@@ -10,7 +10,7 @@ public class MovieMapper {
     public MovieResponseDto toDto(Movie movie) {
         MovieResponseDto movieResponseDto = new MovieResponseDto();
         movieResponseDto.setId(movie.getId());
-        movieResponseDto.setTitle(movieResponseDto.getTitle());
+        movieResponseDto.setTitle(movie.getTitle());
         movieResponseDto.setDescription(movie.getDescription());
         return movieResponseDto;
     }
